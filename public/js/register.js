ba80a5cacfec56ca48f463cc97aab7f8c407c25e
Mikko <mikko@markkaset.com>
2021-10-25 16:56:43 +0300

    Register and login layouts done

--- public/js/register.js
+++ public/js/register.js
@@ -4,9 +4,9 @@ function sendData() {
     
     //TODO
     //PUT THESE TO CONFIG FILE!!!!!!!!!!!!
-    var url = "http://localhost:3000/user";
+    var url = "https://mikko-bootcamp-backend.herokuapp.com/user";
     var username = "APIuser";
-    var password = "APIsecretPass";
+    var password = "APIsecretPassPASS";
 
     var base64Credentials = window.btoa(username + ":" + password);
 
@@ -17,6 +17,23 @@ function sendData() {
     var requestParams = {"username":email,"password":psw,"email":email};
     xhttp.send(JSON.stringify(requestParams));
 }
-function login() {
-    console.log("login");
+function sendRequestForAuth() {
+    email = document.getElementById("email").value;
+        
+    //TODO
+    //PUT THESE TO CONFIG FILE!!!!!!!!!!!!
+    var url = "https://mikko-bootcamp-backend.herokuapp.com/user";
+    var username = "APIuser";
+    var password = "APIsecretPassPASS";
+
+    var base64Credentials = window.btoa(username + ":" + password);
+
+    var xhttp = new XMLHttpRequest();
+    xhttp.open("POST", url, true);
+    xhttp.setRequestHeader("content-type", "application/json");
+    xhttp.setRequestHeader("Authorization", "Basic " + base64Credentials);
+    var requestParams = {"username":email,"email":email};
+    xhttp.send(JSON.stringify(requestParams));
+
+    window.open("login.html?info='Wait for confirmation email send to "+email+" and login by your email and password.", "_self");
 }
