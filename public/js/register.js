c23fb6a84a3401843b09e30e77deb2980969aafe
Mikko <mikko@markkaset.com>
2021-10-26 19:54:31 +0300

    Registering the user implemented

--- public/js/register.js
+++ public/js/register.js
@@ -1,39 +1,31 @@
-function sendData() {
-    email = document.getElementById("email").value;
-    psw = document.getElementById("psw").value;
-    
-    //TODO
-    //PUT THESE TO CONFIG FILE!!!!!!!!!!!!
-    var url = "https://mikko-bootcamp-backend.herokuapp.com/user";
-    var username = "APIuser";
-    var password = "APIsecretPassPASS";
-
-    var base64Credentials = window.btoa(username + ":" + password);
-
-    var xhttp = new XMLHttpRequest();
-    xhttp.open("POST", url, true);
-    xhttp.setRequestHeader("content-type", "application/json");
-    xhttp.setRequestHeader("Authorization", "Basic " + base64Credentials);
-    var requestParams = {"username":email,"password":psw,"email":email};
-    xhttp.send(JSON.stringify(requestParams));
-}
 function sendRequestForAuth() {
+    //get the email to be registered
     email = document.getElementById("email").value;
-        
-    //TODO
-    //PUT THESE TO CONFIG FILE!!!!!!!!!!!!
-    var url = "https://mikko-bootcamp-backend.herokuapp.com/user";
-    var username = "APIuser";
-    var password = "APIsecretPassPASS";
 
-    var base64Credentials = window.btoa(username + ":" + password);
+    //any chance to put this on client variables?
+    var url = "https://mikko-bootcamp-backend.herokuapp.com/requestAuth";
 
+    //build the request to REST API
     var xhttp = new XMLHttpRequest();
     xhttp.open("POST", url, true);
     xhttp.setRequestHeader("content-type", "application/json");
-    xhttp.setRequestHeader("Authorization", "Basic " + base64Credentials);
-    var requestParams = {"username":email,"email":email};
+    var requestParams = { "email": email };
     xhttp.send(JSON.stringify(requestParams));
 
-    window.open("login.html?info='Wait for confirmation email send to "+email+" and login by your email and password.", "_self");
+    //wait the execution to be ready or alert an error
+    xhttp.onload = function () {
+        if (xhttp.status != 200) { // analyze HTTP status of the response
+            alert("Something is broken, sorry: " + xhttp.status + ":" + xhttp.statusText); // e.g. 404: Not Found
+        } else if (JSON.parse(xhttp.response).email!==undefined) { // show the result if response contains valid email
+            let registeredEmail = JSON.parse(xhttp.response).email;
+            alert("The user " + registeredEmail + " succesfully registered. Wait for email confirmation."); // response is the server response
+            window.location = 'login.html';
+        } else { //otherwise alert the response
+            alert("Error: "+xhttp.response);
+        }
+    };
+
+    xhttp.onerror = function () { //on other error, show alert
+        alert("Shit happened: "+xhttp.response);
+    };
 }
