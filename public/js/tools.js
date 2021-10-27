63ad58a553b46d10a6811a701595407eccf6373b
Mikko <mikko@markkaset.com>
2021-10-27 16:29:47 +0300

    Added main page and implemented authentication more

--- public/js/tools.js
+++ public/js/tools.js
@@ -1,30 +1,79 @@
-function sendRequestForAuth() {
+function send(action) {
+
+    if (action == "login") goTo = "login.html";
+    else if (action == "register") goTo = "index.html";
+
     //get the email to be registered
     email = document.getElementById("email").value;
-
+    if (action == "login") {
+        pass = document.getElementById("password").value;
+        apiUsername = document.getElementById("apiUsername").value;
+        apiPassword = document.getElementById("apiPassword").value;
+        window.sessionStorage.setItem("apiUsername", apiUsername);
+        window.sessionStorage.setItem("apiPassword", apiPassword);
+        window.sessionStorage.setItem("email", email);
+        window.sessionStorage.setItem("password", pass);
+    }
     //any chance to put this on client variables?
-    var url = "https://mikko-bootcamp-backend.herokuapp.com/requestAuth";
+    if (action == "register") url = "https://mikko-bootcamp-backend.herokuapp.com/requestAuth";
+    else if (action == "login") url = "https://mikko-bootcamp-backend.herokuapp.com/user/auth";
 
     //build the request to REST API
     var xhttp = new XMLHttpRequest();
     xhttp.open("POST", url, true);
     xhttp.setRequestHeader("content-type", "application/json");
-    var requestParams = { "email": email };
+    if (action == "register") var requestParams = { "email": email };
+    else if (action == "login") {
+        var requestParams = { "username": email, "password": pass };
+        xhttp.setRequestHeader('Authorization', "Basic " + window.btoa(apiUsername + ":" + apiPassword));
+    }
     xhttp.send(JSON.stringify(requestParams));
 
     //wait the execution to be ready or alert an error
     xhttp.onload = function () {
         if (xhttp.status != 200) { // analyze HTTP status of the response
-            window.location="index.html?errorMessage='Something is broken, sorry: " + xhttp.status + ":" + xhttp.statusText + "'"; //show error on page
-        } else if (JSON.parse(xhttp.response).email!==undefined) { // show the result if response contains valid email
-            let registeredEmail = JSON.parse(xhttp.response).email;
-            window.location = "login.html?message='The user " + registeredEmail + " succesfully registered. Wait for email confirmation.'"; //show message on page
-        } else { //if email parameter is undefined, it is already in db
-            window.location="index.html?errorMessage='The email is already registered."; //show error on page
+            window.location = goTo + "?errorMessage='I am sorry: " + xhttp.status + ":" + xhttp.statusText + "'"; //show error on page
+        } else if (action == "register" && JSON.parse(xhttp.responseText).email == undefined) { //if email parameter is undefined, it is already in db
+            window.location = "index.html?errorMessage='The email is already registered.'"; //show error on page
+        } else if (action == "register" && JSON.parse(xhttp.responseText).email != undefined) { // show the result if response contains valid email
+            window.location = "login.html?message='The user " + JSON.parse(xhttp.responseText).email + " succesfully registered. Wait for email confirmation.'"; //show message on page
+        } else if (action = "login" && xhttp.status == 200) {
+            if (xhttp.responseText=="true") window.location = "main.html"; //open the main page after succesful login
+            else alert(xhttp.responseText);
+        } else {
+            //in any other case, just show message on page
+            window.location = goTo + "?message='Server responded:" + xhttp.status + ":" + xhttp.statusText + "'";
         }
     };
 
     xhttp.onerror = function () { //on other error, show alert
-        window.location="index.html?errorMessage='Something is broken, sorry: " + xhttp.status + ":" + xhttp.statusText + "'"; //show error on page
+        window.location = goTo + "?errorMessage='I am sorry:" + xhttp.status + ":" + xhttp.statusText + "'";
     };
+}
+
+function getUser() {
+    //build the request to REST API
+    var xhttp = new XMLHttpRequest();
+    url = "https://mikko-bootcamp-backend.herokuapp.com/user";
+    xhttp.open("GET", url);
+    xhttp.setRequestHeader("content-type", "application/json");
+    xhttp.setRequestHeader('Authorization', "Basic " + window.btoa(window.sessionStorage.getItem("apiUsername") + ":" + window.sessionStorage.getItem("apiPassword")));
+    xhttp.send();
+    //wait the execution to be ready or alert an error
+    xhttp.onload = function () {
+        if (xhttp.status != 200) { // analyze HTTP status of the response
+            return "I am sorry: " + xhttp.status + ":" + xhttp.statusText; //show error on page
+        } else {
+            //in any other case, just show the results
+            return "results: "+JSON.parse(xhttp.response);
+        }
+    };
+    xhttp.onerror = function () { //on other error, show alert
+        return "'I am sorry:" + xhttp.status + ":" + xhttp.statusText + "'";
+    };
+}
+
+function checkLogin() {
+    if (window.sessionStorage.getItem("email")=="") return false;
+    else return true;
 }
